44fb56314ac36ee33307ecb49be8fcb21501905e
Dileep Raj <dileepraj2811@gmail.com>
2026-06-09 19:08:21 +0530

    Added script.js

---
+++ script.js
@@ -0,0 +1,42 @@
+console.log("Hello!");
+
+let name = "Dileep";
+const age = 26;
+var city = "Kochi";
+console.log(name, age, city);
+
+let num1 = 10, num2 = 5;
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(2 ** 3);
+console.log(10 % 3);
+
+let greeting = "Hello";
+let person = "World";
+console.log(greeting + " + person);
+console.log(`Hi ${name}, you are ${age}`);
+console.log(name.length);
+console.log(name.toUpperCase());
+console.log("javascript".toLowerCase());
+
+let x = 5;
+console.log(x);
+x = 10;
+console.log(x);
+
+let score = 100;
+console.log("Your score is: " + score);
+
+let a = 1, b = 2, c = 3;
+console.log(a, b, c);
+
+let num = 50;
+console.log(num.toString());
+
+
+console.log(true, false);
+
+
+console.log("Task complete! 25 examples done.");
