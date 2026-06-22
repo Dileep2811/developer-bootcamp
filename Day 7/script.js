3c685421d9f6c4bd84d8bc57fddc1c83ac629ab0
Dileep Raj <dileepraj2811@gmail.com>
2026-06-22 13:13:09 +0530

    Complete responsive portfolio with HTML, CSS, JS form validation

---
+++ Day 7/script.js
@@ -0,0 +1,11 @@
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+  e.preventDefault();
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  
+  if (name === '' || email === '') {
+    alert('Please fill out all fields');
+  } else {
+    alert('Message sent!');
+  }
+});
